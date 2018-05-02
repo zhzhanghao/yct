2d09342843c2ee36eee41cce9f8769ccbbdd8247
lmm <295048582@qq.com>
2018-05-02 11:21:59 +0800

    测试

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: 'Hello World111',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
